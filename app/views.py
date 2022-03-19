7c4f46c64845e271d986e4c16b73ab3b494be0d4
neongm <chura2013c@gmail.com>
2022-03-19 18:15:41 +0500

    fixin fucking statsCollector class

--- app/views.py
+++ app/views.py
@@ -49,6 +49,15 @@ def index(req):
     p_status = p.wait()
 
     # proccessing data
+    td = """Name                     Remote IP                  Virtual IP      Bytes Received      Bytes Sent      Last Seen
+neongm_main_desktop      00.000.000.00:00000        10.6.0.2        211MiB              1.7GiB          Mar 18 2022 - 12:19:22
+neongm_android_1         00.000.000.00:00000        10.6.0.3        462MiB              1.6GiB          Mar 19 2022 - 10:36:14
+ars_peer_poland          00.000.000.00:00000        10.6.0.4        500MiB              5.1GiB          Mar 19 2022 - 09:28:34
+primary                  (none)                     10.6.0.5        0B                  0B              (not yet)
+mobile                   (none)                     10.6.0.6        0B                  0B              (not yet)
+grig_mobile              00.000.000.00:00000        10.6.0.7        114MiB              2.1GiB          Mar 19 2022 - 10:37:18
+neongm_laptop            00.000.000.00:00000        10.6.0.8        37MiB               127MiB          Mar 19 2022 - 10:35:34
+grig_desktop             (none)                     10.6.0.9        0B                  0B              (not yet)"""
     sc = statsCollector(output.decode('utf-8'))
     sc.process_data()
 
@@ -57,12 +66,11 @@ def index(req):
         'uptimeDays': round(uptime()/(3600*24), 2),
         'uptimeHours': round(uptime()/3600, 2),
         'uptimeSeconds': round(uptime(), 2),
-        'receivedTraffic': sc.get_received_traffic(),
-        'sentTraffic': sc.get_sent_traffic(),
-        'fullTraffic': sc.get_full_traffic(),
         'peerCount': sc.get_peer_count(),
         'activePeerCount': sc.get_active_peer_count(),
-
+        'receivedTraffic': round(sc.get_received_traffic(), 2),
+        'sentTraffic': round(sc.get_sent_traffic(), 2),
+        'fullTraffic': round(sc.get_full_traffic(), 2),
     }
     return render(req, 'index.html', context)
 
