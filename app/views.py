22fa2fcefd604a28410eeedacfdb5e1cea03133a
neongm <chura2013c@gmail.com>
2022-03-19 11:41:22 +0500

    initial commit

---
+++ app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from uptime import uptime
+# Create your views here.
+
+def index(req):
+    context = {
+        'title': 'wireguard vps server',
+        'uptimeHours': round(uptime()/(60*60*60), 2),
+        'uptimeSeconds': round(uptime(), 2)
+    }
+    return render(req, 'index.html', context)
