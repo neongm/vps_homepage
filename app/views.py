14090a793b1180cd0e68f5e98511196dc6c6f5d4
neongm <chura2013c@gmail.com>
2022-03-19 13:16:46 +0500

    calculations fix

--- app/views.py
+++ app/views.py
@@ -5,8 +5,8 @@ from uptime import uptime
 def index(req):
     context = {
         'title': 'wireguard vps server',
-        'uptimeDays': round(uptime()/(60*60*60), 2),
-        'uptimeHours': round(uptime()/(60*60), 2),
+        'uptimeDays': round(uptime()/(3600*24), 2),
+        'uptimeHours': round(uptime()/3600, 2),
         'uptimeSeconds': round(uptime(), 2)
     }
     return render(req, 'index.html', context)
