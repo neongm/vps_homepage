fed0e969010587988cb7cbd07a7a37b9665fddcd
neongm <chura2013c@gmail.com>
2022-03-22 12:04:05 +0500

    rewritten statsCollector, added userStats class

--- app/tests.py
+++ app/tests.py
@@ -1,20 +1,22 @@
 from views import statsCollector
 
 TESTING_STATS = """Name                     Remote IP                  Virtual IP      Bytes Received      Bytes Sent      Last Seen
-neongm_main_desktop      00.000.000.00:00000        10.6.0.2        211MiB              1.7GiB          Mar 18 2022 - 12:19:22
-neongm_android_1         00.000.000.00:00000        10.6.0.3        462MiB              1.6GiB          Mar 19 2022 - 10:36:14
+neongm_main_desktop      00.000.000.00:00000        10.6.0.2        211MiB              1.7GiB          Mar 22 2022 - 12:19:22
+neongm_android_1         00.000.000.00:00000        10.6.0.3        462MiB              1.6GiB          Mar 21 2022 - 10:36:14
 ars_peer_poland          00.000.000.00:00000        10.6.0.4        500MiB              5.1GiB          Mar 19 2022 - 09:28:34
 primary                  (none)                     10.6.0.5        0B                  0B              (not yet)
 mobile                   (none)                     10.6.0.6        0B                  0B              (not yet)
 grig_mobile              00.000.000.00:00000        10.6.0.7        114MiB              2.1GiB          Mar 19 2022 - 10:37:18
 neongm_laptop            00.000.000.00:00000        10.6.0.8        37MiB               127MiB          Mar 19 2022 - 10:35:34
-grig_desktop             (none)                     10.6.0.9        0B                  0B              (not yet)"""
+grig_desktop             (none)                     10.6.0.9        0B                  0B              (not yet)
+: somethin something :"""
 
 st = statsCollector(data=TESTING_STATS)
-st.process_data()
+users = st.get_per_user_stats();
 
 assert st.get_received_traffic() == 1324/1024, f"received traffic calculation failed, true = {1324/1024}, got = {st.get_received_traffic()}"
 assert st.get_sent_traffic() ==  1.7+1.6+5.1+2.1+127/1024, f"sent traffic calculation failed, true = {1.7+1.6+5.1+2.1+127/1024}, got = {st.get_received_traffic()}"
 assert round(st.get_full_traffic(), 2) == round(1324/1024 + 1.7+1.6+5.1+2.1+127/1024, 2), f"full traffic calculation failed, true = {round(1324/1024 + 1.7+1.6+5.1+2.1+127/1024, 2)}, got = {round(st.get_full_traffic(), 2)}"
 assert st.get_peer_count() == 8, f"user count failed, expected {8}, got {st.get_peer_count()}"
 assert st.get_active_peer_count() == 5, f"active user count failed, expected {5}, got {st.get_active_peer_count()}"
+for user in users: print(f"{user.get_user_name()}\n       virtual ip: {user.get_virtual_ip()}\n       full traffic: {round(user.get_traffic_full(), 2)} GiB\n       sent traffic: {round(user.get_traffic_sent(), 2)} GiB\n       received traffic: {round(user.get_traffic_received(), 2)} GiB\n       active at least once: {user.used_at_least_once()}\n       active recently: {user.is_active()}\n       last seen date: {user.get_last_use_date()}")
--- app/views.py
+++ app/views.py
@@ -3,56 +3,115 @@ from uptime import uptime
 import subprocess
 import re
 from random import choice
+from datetime import datetime
 # Create your views here.
+
+class userStats():
+    def __init__(self, user_data_arr):
+        self.data = user_data_arr
+        self.traffic_sent = self.get_traffic_sent()
+
+    def _read_traffic_at_index(self, index):
+        if '0B' in self.data[index]: return 0
+        if 'GiB'in self.data[index]: return float(self.data[index].replace('GiB', ''))
+        if 'MiB'in self.data[index]: return float(self.data[index].replace('MiB', ''))/1024
+        assert False, f"method _read_traffic_at_index() didn't find the traffic data at index {index} in self.data, got {self.data[index]}\nself.data = {self.data}"
+    
+    def get_traffic_sent(self): #id of bytes sent is 4, received 3
+        return self._read_traffic_at_index(4)
+    
+    def get_traffic_received(self):
+        return self._read_traffic_at_index(3)
+
+    def get_traffic_full(self):
+        return self.get_traffic_sent() + self.get_traffic_received()
+
+    def get_user_name(self):
+        return self.data[0]
+
+    def get_virtual_ip(self):
+        return self.data[2]
+
+    def get_last_use_date(self):
+        if len(self.data) <= 7: return "Never"
+        return f"{self.data[5]} {self.data[6]} {self.data[7]}"
+
+    def used_at_least_once(self):
+        if self.get_traffic_received() == 0: return False
+        return True
+
+    def is_active(self): # very bad implementataion, hope it at least works TODO fix is_active
+        date_string = self.data[6] 
+        if not date_string.isdigit() : return False
+        else: user_date = int(date_string)
+        if date_string.isdigit(): 
+            current_date = datetime.now().day
+            if current_date - user_date <= 1: return True
+            return False
+
+    @staticmethod
+    def is_valid(user_data_arr):
+        if (len(user_data_arr) < 4): return False
+        if "10.6.0" in user_data_arr[2]: return True
 
 
 class statsCollector():
     def __init__(self, data = []):
-        self.data = data
+        self.raw_data = data
+        self.data = []
         self.traffic_received = 0
         self.traffic_sent = 0
-    
-    def process_data(self, data = []):
-        if len(self.data): data_array = self.data.split('\n')
-        else: data_array = data.split('\n')
-        self.data = [user_data.split() for user_data in data_array][2:-2:]
-        
-    
+        self.per_user_stats = []
+        self._process_per_user_stats()
+
+    def _process_per_user_stats(self):
+        data_array = self.raw_data.split('\n')
+        self.data = [user_data.split() for user_data in data_array]
+        self.per_user_stats = [userStats(user_data) for user_data in self.data if userStats.is_valid(user_data)]
+
     def get_received_traffic(self):
-        if self.traffic_received == 0:
-            mib = sum([float(d[3].replace('MiB', ''))/1024 for d in self.data if 'MiB' in d[3]])
-            gib = sum([float(d[3].replace('GiB', '')) for d in self.data if 'GiB' in d[3]])
-            self.traffic_received = mib + gib
-        return self.traffic_received
+        return sum([user_data.get_traffic_received() for user_data in self.per_user_stats])
 
     def get_sent_traffic(self):
-        if self.traffic_sent == 0:
-            mib = sum([float(d[4].replace('MiB', ''))/1024 for d in self.data if 'MiB' in d[4]])
-            gib = sum([float(d[4].replace('GiB', '')) for d in self.data if 'GiB' in d[4]])
-            self.traffic_sent = mib + gib
-
-        return self.traffic_sent
+        return sum([user_data.get_traffic_sent() for user_data in self.per_user_stats])
 
     def get_full_traffic(self):
         return self.get_sent_traffic() + self.get_received_traffic()
 
     def get_peer_count(self):
-        return len(self.data)-1
+        return len(self.per_user_stats)
     
     def get_active_peer_count(self):
-        return self.get_peer_count() - len([d[4] for d in self.data if "0B" in d[4]])
+        return len([' ' for user_data in self.per_user_stats if user_data.used_at_least_once()])
+    
+    def get_peers_used_recently_count(self):
+        return len([' ' for user_data in self.per_user_stats if user_data.is_active()])
+
+    def get_per_user_stats(self): return self.per_user_stats 
 
 
 def index(req):
-    catMessages = ["have a good day", "rusk rusk rusk rusk", "the table", "how does it work", "boo", "you're awesome", "code works", "you're secure", "furfur", "meow"]
+    catMessages = ["have a good day", "rusk rusk rusk rusk", "the table", "how does it work", "boo", "you're awesome", "code works", "you're secure", "furfur", "meow", "yes, the кот"]
     # getting data
     p = subprocess.Popen(["pivpn -c"], stdout=subprocess.PIPE, shell=True)
     (output, err) = p.communicate()
     p_status = p.wait()
     
     # proccessing data
-    sc = statsCollector(output.decode('utf-8'))
-    sc.process_data()
+    TESTING_STATS = """Name                     Remote IP                  Virtual IP      Bytes Received      Bytes Sent      Last Seen
+neongm_main_desktop      00.000.000.00:00000        10.6.0.2        211MiB              1.7GiB          Mar 22 2022 - 12:19:22
+neongm_android_1         00.000.000.00:00000        10.6.0.3        462MiB              1.6GiB          Mar 21 2022 - 10:36:14
+ars_peer_poland          00.000.000.00:00000        10.6.0.4        500MiB              5.1GiB          Mar 19 2022 - 09:28:34
+primary                  (none)                     10.6.0.5        0B                  0B              (not yet)
+mobile                   (none)                     10.6.0.6        0B                  0B              (not yet)
+grig_mobile              00.000.000.00:00000        10.6.0.7        114MiB              2.1GiB          Mar 19 2022 - 10:37:18
+neongm_laptop            00.000.000.00:00000        10.6.0.8        37MiB               127MiB          Mar 19 2022 - 10:35:34
+grig_desktop             (none)                     10.6.0.9        0B                  0B              (not yet)
+: somethin something :"""
+    #sc = statsCollector(output.decode('utf-8'))
+    sc = statsCollector(TESTING_STATS)
+    per_user_stats = sc.get_per_user_stats()
+
 
     context = {
         'title': 'wireguard vps server',
